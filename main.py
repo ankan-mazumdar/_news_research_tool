75b20dee5869891084eeffa31f073206391a47a4
Ankan Mazumdar <ankanmazumdar2016@gmail.com>
2024-09-16 13:02:15 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import streamlit as st
+import time
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env (especially openai api key)
+
+st.title("RockyBot: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(2):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+index_file_path = "index.pkl" #copy the file from notebooks/vectorDB/index.pkl
+vectorstore_file_path = "vectorstore_openai.faiss"  # Change the file name here
+
+main_placeholder = st.empty()
+llm = OpenAI(temperature=0.9, max_tokens=500)
+
+if process_url_clicked:
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    embeddings = OpenAIEmbeddings()
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    # Save the FAISS index to a file
+    vectorstore_openai.save_local(vectorstore_file_path)  # Use FAISS's save_local method
+
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(index_file_path) and os.path.exists(vectorstore_file_path):
+        # Load the vector index directly from the saved file
+        loaded_vectorstore = FAISS.load_local(vectorstore_file_path, OpenAIEmbeddings(), allow_dangerous_deserialization=True)  # Use FAISS's load_local method
+        chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=loaded_vectorstore.as_retriever())
+        result = chain({"question": query}, return_only_outputs=True)
+        # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+        st.header("Answer")
+        st.write(result["answer"])
+
+        # Display sources, if available
+        sources = result.get("sources", "")
+        if sources:
+            st.subheader("Sources:")
+            sources_list = sources.split("\n")  # Split the sources by newline
+            for source in sources_list:
+                st.write(source)
